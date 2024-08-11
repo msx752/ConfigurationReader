2aafe79c3db8aed0e2ffca3be1b94687699e54e6
Mustafa Salih ASLIM <salih@aslim.net>
2024-08-11 23:52:45 +0300

    project is updated

---
+++ src/ConfigurationLib.LogService/Controllers/ConfigurationController.cs
@@ -0,0 +1,35 @@
+﻿using ConfigurationLib.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationLib.LogService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        IConfigurationReader _configurationReader;
+        IConfiguration _configuration;
+        public ConfigurationController(IConfigurationReader configurationReader, IConfiguration configuration)
+        {
+            _configurationReader = configurationReader;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            object obj = new
+            {
+                CurrentApplicationName= _configuration["AppName"],
+                RefreshInterval = _configuration["RefreshTimerIntervalInMs"],
+                SiteName = _configurationReader.GetValue<string>("SiteName"),
+                MaxItemCount = _configurationReader.GetValue<int>("MaxItemCount"),
+                IsBasketEnabled = _configurationReader.GetValue<bool>("IsBasketEnabled"),
+                IsloggingEnabled = _configurationReader.GetValue<bool>("IsloggingEnabled"),
+            };
+
+            return new JsonResult(obj, new System.Text.Json.JsonSerializerOptions() { WriteIndented = true });
+        }
+    }
+}
